bb32ea01244d4234b14bcb7c7ea9b9edb521d0dd
chaehyeo-n <parkche03@naver.com>
2024-10-04 15:35:47 +0900

    Fix: 암호화 이전 버전 복원...

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -2,13 +2,12 @@ import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export const getQRCode = async (encryptedId) => {
+export const getQRCode = async (id) => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/api/qr/${encryptedId}/`);
+        const response = await axios.get(`${API_BASE_URL}/api/photos/${id}`);
         return response.data;
     } catch (error) {
         console.error('큐알코드 실패했슴다 : ', error);
         throw error;
     }
 };
-
