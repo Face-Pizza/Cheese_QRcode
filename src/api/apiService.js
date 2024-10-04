3c222ad62a638142729996e32bf03bd30a3625a3
chaehyeo-n <parkche03@naver.com>
2024-10-04 14:31:59 +0900

    Fix: qr api url 수정

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -4,7 +4,7 @@ const API_BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const getQRCode = async (encryptedId) => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/api/photos/qr/${encryptedId}/`);
+        const response = await axios.get(`${API_BASE_URL}/api/qr/${encryptedId}/`);
         return response.data;
     } catch (error) {
         console.error('큐알코드 실패했슴다 : ', error);
