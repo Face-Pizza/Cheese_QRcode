0fb5b144c26ef6bd50e0dfb40084fb7dbfbee910
chaehyeo-n <parkche03@naver.com>
2024-10-04 13:27:15 +0900

    Fix: qr id 암호화

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export const getQRCode = async (id) => {
+export const getQRCode = async (encryptedId) => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/api/photos/${id}`);
+        const response = await axios.get(`${API_BASE_URL}/api/photos/qr/${encryptedId}/`);
         return response.data;
     } catch (error) {
         console.error('큐알코드 실패했슴다 : ', error);
